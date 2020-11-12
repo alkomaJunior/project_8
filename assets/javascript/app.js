72d4b8611e87276d1b8a8c1024222f33967c7f32
bigboss-oualid <oualid.boulatar@gmail.com>
2020-11-12 23:41:28 +0100

    style: install & setpup webpack encore

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import '../styles/app.css';
+import '../styles/shop-homepage.css';
+import 'jquery/dist/jquery.js';
+import 'glyphicons-only-bootstrap/fonts/glyphicons-halflings-regular.eot';
+import 'glyphicons-only-bootstrap/fonts/glyphicons-halflings-regular.svg';
+import 'glyphicons-only-bootstrap/fonts/glyphicons-halflings-regular.ttf';
+import 'glyphicons-only-bootstrap/fonts/glyphicons-halflings-regular.woff';
+import 'glyphicons-only-bootstrap/fonts/glyphicons-halflings-regular.woff2';
+import 'bootstrap/dist/js/bootstrap.min';
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
+//import $ from 'jquery';
+
+//console.log('Hello Webpack Encore! Edit me in assets/app.js');
