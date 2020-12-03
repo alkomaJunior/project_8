485ccd4970cad6699742224b4aef31003e305281
bigboss-oualid <oualid.boulatar@gmail.com>
2020-12-03 17:52:54 +0100

    fix: show accomplished tasks list & unaccomplished tasks list

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -3,9 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Task;
-use App\Entity\User;
 use App\Form\TaskType;
-use DateTime;
+use App\Repository\TaskRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,13 +16,27 @@ use Symfony\Component\Routing\Annotation\Route;
 class TaskController extends AbstractController
 {
     /**
-     * @Route("/tasks", name="task_list")
+     * @var EntityManagerInterface
      */
-    public function listAction(): Response
-    {
-        $tasks = $this->getDoctrine()->getRepository(Task::class)->findAll();
+    private $entityManager;
 
-        return $this->render('task/list.html.twig', ['tasks' => $tasks]);
+    public function __construct( EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/tasks", name="task_list_all")
+     * @Route("/tasks/done/{!isDone}", name="task_list", requirements={"isDone"="true|false"})
+     */
+    public function listAction(TaskRepository $repository, ?string $isDone = null): Response
+    {
+        $tasks = $repository->findTasks($isDone);
+
+        return $this->render('task/list.html.twig', [
+            'tasks' => $tasks,
+            'isDone' => $isDone,
+        ]);
     }
 
     /**
@@ -31,49 +44,41 @@ class TaskController extends AbstractController
      *
      * @return RedirectResponse|Response
      */
-    public function createAction(Request $request, EntityManagerInterface $entityManager): Response
+    public function createAction(Request $request): Response
     {
-        /** @var User $user */
-        $user = $this->getUser();
         $task = new Task();
         $form = $this->createForm(TaskType::class, $task);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // TODO: refactoring
-            $task->setUser($user)->setCreatedAt(new DateTime());
-            $entityManager->persist($task);
-            $entityManager->flush();
+            $this->entityManager->persist($task);
+            $this->entityManager->flush();
 
-            $this->addFlash('success', '<strong>Superbe !</strong> La tâche a été bien été ajoutée.');
+            $this->addFlash('success', 'La tâche a été bien été ajoutée.');
 
-            return $this->redirectToRoute('task_list');
+            return $this->redirectToRoute('task_list', ['isDone' => 'false']);
         }
 
         return $this->render('task/create.html.twig', ['form' => $form->createView()]);
     }
 
     /**
-     * @Route("/tasks/{id}/edit", name="task_edit")
+     * @Route("/tasks/{id}/edit", name="task_edit", requirements={"id"="\d+"})
      *
      * @return RedirectResponse|Response
      */
-    public function editAction(
-        Task $task,
-        Request $request,
-        EntityManagerInterface $entityManager
-    ): Response {
+    public function editAction(Task $task, Request $request): Response
+    {
         $form = $this->createForm(TaskType::class, $task);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
+            $this->entityManager->flush();
+            $this->addFlash('success', 'La tâche a bien été modifiée.');
 
-            $this->addFlash('success', '<strong>Superbe !</strong> La tâche a bien été modifiée.');
-
-            return $this->redirectToRoute('task_list');
+            return $this->redirect($request->request->get('referer'));
         }
 
         return $this->render('task/edit.html.twig', [
@@ -83,48 +88,48 @@ class TaskController extends AbstractController
     }
 
     /**
-     * @Route("/tasks/{id}/toggle", name="task_toggle")
+     * @Route("/tasks/{id}/toggle", name="task_toggle", requirements={"id"="\d+"})
      *
      * @return RedirectResponse
      */
-    public function toggleTaskAction(Task $task, EntityManagerInterface $entityManager): Response
+    public function toggleTaskAction(Task $task, Request $request): RedirectResponse
     {
-        $task->toggle(!$task->isDone());
-        $entityManager->flush();
+        $flag = $task->isDone();
 
-        $this->addFlash('success', $this->taskFlash($task->isDone(), $task->getTitle()));
+        $task->toggle(!$flag);
+        $this->entityManager->flush();
 
-        return $this->redirectToRoute('task_list');
+        $this->addFlash(
+            'success',
+            sprintf(
+                'La tâche %s a bien été marquée comme %s faite.',
+                $task->getTitle(),
+                ($flag) ? 'n\'est pas encore' : ''
+            )
+        );
+
+        return $this->redirect($request->headers->get('referer'));
     }
 
     /**
-     * @Route("/tasks/{id}/delete", name="task_delete")
+     * @Route("/tasks/{id}/delete", name="task_delete", requirements={"id"="\d+"})
      * @IsGranted("DELETE", subject="task")
      *
      * @return RedirectResponse
      */
-    public function deleteTaskAction(Task $task, EntityManagerInterface $entityManager, Request $request): Response
+    public function deleteTaskAction(Task $task, Request $request): RedirectResponse
     {
         if ($this->isCsrfTokenValid('delete'.$task->getId(), $request->get('_token'))) {
-            /*$entityManager->remove($task);
-            $entityManager->flush();*/
+            $this->entityManager->remove($task);
+            $this->entityManager->flush();
 
-            $this->addFlash('success', '<strong>Superbe !</strong> La tâche a bien été supprimée.');
-        } else {
-            $this->addFlash('warning', 'la tâche n\'a pas été supprimée.');
+            $this->addFlash('success', 'La tâche a bien été supprimée.');
+
+            return $this->redirect($request->headers->get('referer'));
         }
 
-        return $this->redirectToRoute('task_list');
-    }
+        $this->addFlash('warning', 'la tâche n\'a pas été supprimée. le token n\'est pas valid');
 
-    // TODO: Refactoring
-    private function taskFlash(bool $isDone, string $title): string
-    {
-        if ($isDone) {
-            return sprintf('<strong>Superbe !</strong> La tâche %s a bien été marquée comme faite.', $title);
-        }
-
-        return sprintf('<strong>Superbe !</strong> La tâche %s a bien été marquée comme n\'est pas encore faite.',
-            $title);
+        return $this->redirect($request->headers->get('referer'));
     }
 }
---
+++ src/Event/TaskSubscriber.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * ToDoAndCo Project
+ * Copyright (c) 2020 BigBoss 2020.  BigBoss Oualid
+ * mailto: <bigboss@it-bigboss.de>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code
+ * Inc., Munich, Germany.
+ */
+
+namespace App\Event;
+
+use App\Entity\Task;
+use App\Entity\User;
+use DateTime;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Events;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Symfony\Component\Security\Core\Security;
+
+class TaskSubscriber implements EventSubscriber
+{
+    /**
+     * @var Security
+     */
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    /**
+     * Returns an array of events this subscriber wants to listen to.
+     *
+     * @return string[]
+     */
+    public function getSubscribedEvents(): array
+    {
+        return [
+            Events::prePersist,
+        ];
+    }
+
+    public function prePersist(LifecycleEventArgs $args): void
+    {
+        $entity = $args->getObject();
+
+        if (!$entity instanceof Task) {
+            return;
+        }
+        $entity->setUser($this->security->getUser())->setCreatedAt(new DateTime());
+    }
+}
