16e1356fd178cd6560b1413b5a45ca03fc281498
bigboss-oualid <oualid.boulatar@gmail.com>
2020-12-07 17:56:43 +0100

    test: install & set up PHPUnit

---
+++ tests/AppTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * ToDoAndCo Project
+ * Copyright (c) 2020 BigBoss 2020.  BigBoss Oualid
+ * mailto: <bigboss@it-bigboss.de>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code
+ * Inc., Munich, Germany.
+ */
+
+namespace App\Tests;
+
+
+use PHPUnit\Framework\TestCase;
+
+class AppTest extends TestCase
+{
+    public function testTestAreWorking()
+    {
+        $this->assertEquals(5, 2+3, "5 was expected to equal 2+3");
+    }
+}
